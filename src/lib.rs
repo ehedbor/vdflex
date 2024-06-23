0ae78fdfc8479c85a2ea6e38d1d204c4528ce3e9
Evan Hedbor <evan@hedbor.org>
2024-06-24 00:32:37 +0200

    Add basic impl for (De)Serialize for KeyValues

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,76 @@ pub mod ser;
 
 pub use error::{Error, Result};
 
+use std::fmt;
+use std::result;
+
 /// Represents all possible KeyValues values.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Value {
     String(String),
     Object(Object),
+}
+
+#[cfg(feature = "std")]
+impl serde::Serialize for Value {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> result::Result<S::Ok, S::Error> {
+        match self {
+            Value::String(string) => string.serialize(serializer),
+            Value::Object(object) => object.serialize(serializer),
+        }
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for Value {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> result::Result<Self, D::Error> {
+        deserializer.deserialize_any(ValueVisitor)
+    }
+}
+
+struct ValueVisitor;
+
+impl<'de> serde::de::Visitor<'de> for ValueVisitor {
+    type Value = Value;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a KeyValues string or object")
+    }
+
+    fn visit_str<E: serde::de::Error>(self, v: &str) -> result::Result<Self::Value, E> {
+        Ok(Value::String(String::from(v)))
+    }
+
+    fn visit_string<E: serde::de::Error>(self, v: String) -> result::Result<Self::Value, E> {
+        Ok(Value::String(v))
+    }
+
+    fn visit_map<A: serde::de::MapAccess<'de>>(
+        self,
+        mut map: A,
+    ) -> result::Result<Self::Value, A::Error> {
+        #[cfg(feature = "preserve_order")]
+        use indexmap::map::Entry;
+        #[cfg(not(feature = "preserve_order"))]
+        use std::collections::btree_map::Entry;
+
+        #[cfg(feature = "preserve_order")]
+        let mut obj = Object::with_capacity(map.size_hint().unwrap_or(0));
+        #[cfg(not(feature = "preserve_order"))]
+        let mut obj = Object::new();
+
+        while let Some((key, value)) = map.next_entry::<String, Value>()? {
+            match obj.entry(key) {
+                Entry::Occupied(mut oe) => {
+                    oe.get_mut().push(value);
+                }
+                Entry::Vacant(ve) => {
+                    ve.insert(vec![value]);
+                }
+            }
+        }
+
+        Ok(Value::Object(obj))
+    }
 }
 
 /// Represents a KeyValues object.
@@ -48,18 +113,13 @@ impl KeyValues {
 
 #[cfg(feature = "std")]
 impl serde::Serialize for KeyValues {
-    fn serialize<S: serde::Serializer>(
-        &self,
-        _serializer: S,
-    ) -> std::result::Result<S::Ok, S::Error> {
-        todo!()
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> result::Result<S::Ok, S::Error> {
+        self.root.serialize(serializer)
     }
 }
 
 impl<'de> serde::Deserialize<'de> for KeyValues {
-    fn deserialize<D: serde::Deserializer<'de>>(
-        _deserializer: D,
-    ) -> std::result::Result<Self, D::Error> {
-        todo!()
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> result::Result<Self, D::Error> {
+        Ok(Self::with_root(Object::deserialize(deserializer)?))
     }
 }
